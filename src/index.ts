94c3678ededacfca8ea9ad8cd7d7b544489ff52b
LoTwT <709937065@qq.com>
2022-05-07 17:55:10 +0800

    feat: useGraphqlQuery and GraphqlResponse

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("ts-starter")
+export const useGraphqlQuery = <T extends string>(
+  resource: string,
+  fields: T[],
+) => ({
+  body: {
+    query: `{${resource}{${fields.join(" ")}}}`,
+  },
+})
+
+export type GraphqlResponse<Resource extends string, Fields extends string> = {
+  data: {
+    [P in Resource]: Record<Fields, any>
+  }
+}
